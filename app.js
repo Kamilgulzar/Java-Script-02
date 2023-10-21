dd0a8edbbaf99797ad51103676e5a1339de137c6
Kamil Gulzar <kamilgulzar289@gmail.com>
2023-10-21 04:02:55 -0700

    Update app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+// Chapter 5
+
+// Q1
+// let num1 = 3
+// let num2 = 5
+// document.write("Sum of 3 and 5 is"+  " " ,num1+num2)
+
+// Q2
+// let num1 = 13
+// let num2 = 12
+// document.write("Sum of 13 and 12 is"+  " " ,num1+num2 +"</br>")
+// document.write("Subtraction of 13 and 12 is"+ " ", num1-num2 + "</br>")
+// document.write("Multiplication of 13 and 12 is"+ " ", num1*num2 + "</br>")
+// document.write("Division of 13 and 12 by 5 is"+ " ", (num1+num2)/5 + "</br>")
+// document.write("Modulus of 13 and 12 is"+ " ", (num1+num2)%5 + "</br>")
+
+// Q3
+var num1;
+
+
+
