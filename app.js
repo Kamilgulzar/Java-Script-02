2b6e3c1cb6953d23cdcac2c4679adcc73742543d
Kamil Gulzar <kamilgulzar289@gmail.com>
2023-10-21 08:02:13 -0700

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,71 @@
 // document.write("Modulus of 13 and 12 is"+ " ", (num1+num2)%5 + "</br>")
 
 // Q3
-var num1;
+// var num1;
+// document.write("Document after variable declaration is"+ " ",num1 + "</br>")
+// var num2 = 5
+// document.write("Initial value:", num2 + "</br>",)
+// num2++
+// document.write("Value after increment:"+ " ", num2++ +"</br>") 
+// document.write("Value after addition:"+" ",num2+6 +"</br>")
+// num2 = 13
+// num2--
+// document.write("value after decrement:"+" ",num2-- +"</br>")
+// document.write("The remainder is", num2%11 + "</br>")
 
 
+// Q4
+// var movie_ticket = 600+"pkr"
+// totalCost = 600*5
+// document.write("Total cost to buy 5 tickets to a movie is"+ " ", totalCost, "PKR")
+
+// Q5
+// document.write("Table of 4"+ "</br>")
+// document.write("4*1"+"=",4)
+// document.write("4*2"+"=",8)
+// document.write("4*3"+"=",12)
+// document.write("4*4"+"=",16)
+// document.write("4*5"+"=",20)
+// document.write("4*6"+"=",24)
+// document.write("4*7"+"=",28)
+// document.write("4*8"+"=",32)
+// document.write("4*9"+"=",36)
+// document.write("4*10"+"=",40)
+
+// Q6
+// var celsius = 25
+// F = (celsius*9/5)+32
+// document.write(25,"°C"+" is" + " ",F+"F" + "</br>")
+// var farenheit = 70
+// C = (farenheit-32)*5/9 
+// document.write(70,"°F"+" is" + " ",C+"F")
+
+// Q7
+// document.write("<b>"+"Shopping Cart"+"</b>" + "</br>")
+// var price_item = 650
+// var Qt1 = 3
+// document.write("Price of 1 item is"+ " ",price_item + "</br>")
+// document.write("Price of item 2 is"+" ",Qt1 + "</br>")
+// document.write()
+// var price_item2 = 100
+// var Qt2 = 7
+// document.write("Quantity of item 2 is"+" ",Qt2 + "</br>")
+// var sc = 100 
+// document.write("Total cost of your order is"+ " ", price_item*3+price_item2*7+sc)
+
+// Q8
+// document.write("<b>"+"Mark Sheet"+"</b>"+"</br>")
+// var totalMarks = 980
+// var marksObtained = 804 
+// document.write("Total Marks: ",totalMarks + "</br>")
+// document.write("Obtained Marks: ",marksObtained + "</br>")
+// document.write("Total percentage %:"+ " ", marksObtained/totalMarks*100)
+
+// Q9
+// document.write("<b>"+"Currency in PKR"+"</b>"+"</br>")
+// var tusd = 10
+// var tsar = 25
+// var sar1 = 28
+// var usd1 = 104.80
+// document.write("Total currency in PKR:"+ "" ,(tusd*usd1)+(tsar*sar1))
 
